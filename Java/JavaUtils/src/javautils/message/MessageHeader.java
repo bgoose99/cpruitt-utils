0125c51b6544c59b0c202a9bec2700fff72c22b4
charlesandmarypruitt@gmail.com <charlesandmarypruitt@gmail.com@5557aa65-72ec-d561-3f1f-9a833d31c65a>
2011-09-28 19:15:56 +0000

    Got rid of debug statement; comments

--- Java/JavaUtils/src/javautils/message/MessageHeader.java
+++ Java/JavaUtils/src/javautils/message/MessageHeader.java
@@ -3,6 +3,9 @@ package javautils.message;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 
+/*******************************************************************************
+ * This class represents a simple message header.
+ ******************************************************************************/
 public class MessageHeader implements IMessageHeader
 {
     public static final long LENGTH = 16;
@@ -11,11 +14,21 @@ public class MessageHeader implements IMessageHeader
     private MessageType messageType;
     private long messageLength;
 
+    /***************************************************************************
+     * Constructor
+     **************************************************************************/
     public MessageHeader()
     {
         this( 0, MessageType.CHAT, 0 );
     }
 
+    /***************************************************************************
+     * Constructor
+     * 
+     * @param sender
+     * @param type
+     * @param length
+     **************************************************************************/
     public MessageHeader( int sender, MessageType type, long length )
     {
         this.senderHash = sender;
@@ -23,48 +36,84 @@ public class MessageHeader implements IMessageHeader
         this.messageLength = length;
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see javautils.message.IMessageHeader#getSenderHash()
+     */
     @Override
     public int getSenderHash()
     {
         return senderHash;
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see javautils.message.IMessageHeader#setSenderHash(int)
+     */
     @Override
     public void setSenderHash( int hash )
     {
         senderHash = hash;
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see javautils.message.IMessageHeader#getMessageType()
+     */
     @Override
     public MessageType getMessageType()
     {
         return messageType;
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * javautils.message.IMessageHeader#setMessageType(javautils.message.MessageType
+     * )
+     */
     @Override
     public void setMessageType( MessageType type )
     {
         messageType = type;
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see javautils.message.IMessageHeader#getMessageLength()
+     */
     @Override
     public long getMessageLength()
     {
         return messageLength;
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see javautils.message.IMessageHeader#setMessageLength(long)
+     */
     @Override
     public void setMessageLength( long length )
     {
         messageLength = length;
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see javautils.message.IMessageHeader#headerToBinaryStream(java.io.
+     * DataOutputStream)
+     */
     @Override
     public void headerToBinaryStream( DataOutputStream stream )
             throws Exception
     {
-        System.out.println( "DEBUG: header = [" + senderHash + ", "
-                + messageType.name() + ", " + messageLength );
         // sender hash code
         stream.writeInt( senderHash );
 
@@ -75,6 +124,13 @@ public class MessageHeader implements IMessageHeader
         stream.writeLong( messageLength );
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * javautils.message.IMessageHeader#binaryStreamToHeader(java.io.DataInputStream
+     * )
+     */
     @Override
     public IMessageHeader binaryStreamToHeader( DataInputStream stream )
             throws Exception
--- Java/JavaUtils/src/javautils/message/MessageType.java
+++ Java/JavaUtils/src/javautils/message/MessageType.java
@@ -1,21 +1,42 @@
 package javautils.message;
 
+/*******************************************************************************
+ * Enum for different message types.
+ ******************************************************************************/
 public enum MessageType
 {
     HEARTBEAT( 0 ), CHAT( 1 );
 
+    /** This value facilitates serialization. */
     private final int value;
 
+    /***************************************************************************
+     * Constructor
+     * 
+     * @param value
+     **************************************************************************/
     private MessageType( int value )
     {
         this.value = value;
     }
 
+    /***************************************************************************
+     * Returns the value of this message type.
+     * 
+     * @return
+     **************************************************************************/
     public int toValue()
     {
         return value;
     }
 
+    /***************************************************************************
+     * Returns the message type associated with the given value.
+     * 
+     * @param value
+     * @return
+     * @throws IllegalArgumentException
+     **************************************************************************/
     public static MessageType fromValue( int value )
             throws IllegalArgumentException
     {
