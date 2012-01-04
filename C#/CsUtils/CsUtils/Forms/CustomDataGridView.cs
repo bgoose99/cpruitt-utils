eaa9faefd7a7608fe088e88524ae6d83a95d7822
charlespruitt79@gmail.com <charlespruitt79@gmail.com@5557aa65-72ec-d561-3f1f-9a833d31c65a>
2012-01-04 15:34:31 +0000

    Added CsUtils, a C# class library project

---
+++ C#/CsUtils/CsUtils/Forms/CustomDataGridView.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace CsUtils.Forms
+{
+    /// <summary>
+    /// This is a custome DataGridView class that displays row numbers in
+    /// the row header.
+    /// </summary>
+    public class CustomDataGridView : DataGridView
+    {
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public CustomDataGridView()
+        {
+        }
+        
+        /// <summary>
+        /// Custom paint method.
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnRowPostPaint( DataGridViewRowPostPaintEventArgs e )
+        {
+            string strRowNumber = ( e.RowIndex + 1 ).ToString();
+
+            // add leading zeros
+            while( strRowNumber.Length < this.RowCount.ToString().Length )
+                strRowNumber = "0" + strRowNumber;
+
+            // calculate size of string
+            SizeF size = e.Graphics.MeasureString( strRowNumber, this.Font );
+
+            // adjust column width, if necessary
+            if( this.RowHeadersWidth < (int)( size.Width + 20 ) )
+                this.RowHeadersWidth = (int)( size.Width + 20 );
+
+            // get the brush we'll use
+            Brush b = SystemBrushes.ControlText;
+
+            // draw row number
+            e.Graphics.DrawString( strRowNumber, this.Font, b, e.RowBounds.Location.X + 15, e.RowBounds.Location.Y + ( ( e.RowBounds.Height - size.Height ) / 2 ) );
+
+            // call base method
+            base.OnRowPostPaint( e );
+        }
+    }
+}
