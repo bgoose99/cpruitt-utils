e13207ff4e28510e831b4200ab5e4d69bc86dc7d
charlespruitt79@gmail.com <charlespruitt79@gmail.com@5557aa65-72ec-d561-3f1f-9a833d31c65a>
2014-07-14 14:07:49 +0000

    Minor tweak to TimingUtils.

--- C/CppUtils/CppUtils/TimingUtils.cpp
+++ C/CppUtils/CppUtils/TimingUtils.cpp
@@ -13,23 +13,47 @@
 
 namespace TimingUtils
 {
-
+   
    /***************************************************************************
     *
     **************************************************************************/
-   long getSystemMillis( )
+   long getSystemMicros()
    {
 #ifdef _WIN32
       static FILETIME ft;
       static long long now;
       GetSystemTimeAsFileTime( &ft );
       now = (LONGLONG)ft.dwLowDateTime + ( (LONGLONG)( ft.dwHighDateTime ) << 32LL );
-      return (long)( now / 10000 );
+      return (long)( now / 10 ); // 100-nanosecond intervals in a microsecond
 #else
       static struct timeval tv;
       gettimeofday( &tv, 0 );
-      return ( tv.tv_usec + ( tv.tv_sec * 1000000 ) ) / 1000;
+      return ( tv.tv_usec + ( tv.tv_sec * 1000000 ) );
 #endif
+   }
+   
+   /***************************************************************************
+    *
+    **************************************************************************/
+   long getSystemMillis( )
+   {
+      return getSystemMicros() / 1000;
+   }
+   
+   /***************************************************************************
+    *
+    **************************************************************************/
+   void spinSleep( const long &microseconds )
+   {
+      long now = getSystemMicros();
+      long later = now + microseconds;
+      long maxSleep = later - now;
+      
+      while( now < later )
+      {
+         now = getSystemMicros();
+         if( ( later - now ) > maxSleep ) return; // time has elapsed, wake up
+      }
    }
 
 }
